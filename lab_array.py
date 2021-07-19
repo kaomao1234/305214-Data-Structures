d65ee866c602ecc8b541290a7620581724db4952
kaomao1234 <boripats63@nu.ac.th>
2021-07-19 20:56:07 +0700

    add all

--- lab_array.py
+++ lab_array.py
@@ -5,97 +5,98 @@ import time
 
 class Array:
     def __init__(self, *arg: int):
-        self.get_array = arg[0] if type(arg[0]) in (list, tuple) else list(arg)
+        self.iterVal = arg[0] if type(arg[0]) in (list, tuple) else list(arg)
 
     #! Bubble sort method
     @property
-    def bubble_sort(self):
-        for i in range(len(self.get_array)):
+    def BubbleSort(self):
+        for i in range(len(self.iterVal)):
             # todo ตัวแปรที่ใช้หยุด loop เมื่อข้อมูลทุกตัวเรียงลำดับแล้ว
             stop = 0
             # todo loop กระทำ len(list)-1 รอบ เพื่อให้ตัวเลขสามรถจับคู่กัน โดย ไม่ให้ สมาชิกสุดท้ายเป็นตำแหน่งที่ n
-            for j in range(0, len(self.get_array)-1):
+            for j in range(0, len(self.iterVal)-1):
                 # todo ถ้า ตำแหน่งที่ n มีค่ามากว่า n+1
-                if self.get_array[j] > self.get_array[j+1]:
+                if self.iterVal[j] > self.iterVal[j+1]:
                     # todo here_ele คือตัวแปรที่ n, next_ele คือตัวแปรที่ n+1
-                    here_ele, next_ele = self.get_array[j], self.get_array[j+1]
+                    here_ele, next_ele = self.iterVal[j], self.iterVal[j+1]
                     # todo สลับสมาชิก
-                    self.get_array[j] = next_ele
-                    self.get_array[j+1] = here_ele
+                    self.iterVal[j] = next_ele
+                    self.iterVal[j+1] = here_ele
                 # todo เมื่อไม่เข้าเงื่อนไขด้านบน
                 else:
                     stop += 1
-            if stop == len(self.get_array)-1:
+            if stop == len(self.iterVal)-1:
                 break
 
     #! Insertion sort method
     @property
-    def insertion_sort(self):
-        for i in range(1, len(self.get_array)):
-            # todo ins_ele คือตัวแปรที่ใช้สำหรับการ insert โดยให้เริ่มต้นตำแหน่งที่ 1 เสมอ
-            ins_ele = self.get_array[i]
-            # todo left_ele คือ array ที่แบ่งออกมาจากทางด้ายของ ins_ele เพื่อใช้สำหรับการสลับที่
-            left_ele = self.get_array[:self.get_array.index(ins_ele)]
-            # todo left_ele[::-1] คือ reversed สมาชิกที่อยู่ใน array
-            for j in left_ele[::-1]:
-                # todo เปรียบเทียบระหว่าง j(สมาชิกตัวแรกที่อยู่ทางซ้ายของ ins_ele โดยนับจากตัวสุดท้าย) มากกว่า ins_ele
-                if j > ins_ele:
+    def InsertionSort(self):
+        for i in range(1, len(self.iterVal)):
+            # todo comparator คือตัวแปรที่ใช้สำหรับการ insert โดยให้เริ่มต้นตำแหน่งที่ 1 เสมอ
+            comparator = self.iterVal[i]
+            # todo reversed(self.iterVal[:i]) คือ reversed สมาชิกที่อยู่ใน array
+            for j  in self.iterVal[i::-1]:
+                # todo เปรียบเทียบระหว่าง j(สมาชิกตัวแรกที่อยู่ทางซ้ายของ comparator โดยนับจากตัวสุดท้าย) มากกว่า comparator
+                if j > comparator:
                     # todo สลับตำแหน่ง เริ่มสลับตำแหน่ง
-                    self.get_array[self.get_array.index(
-                        ins_ele)], self.get_array[self.get_array.index(j)] = j, ins_ele
+                    self.iterVal[self.iterVal.index(
+                        comparator)], self.iterVal[self.iterVal.index(j)] = j, comparator
 
     #! Selection sort method
     @property
-    def selection_sort(self):
-        for ex in range(len(self.get_array)):
+    def SelectionSort(self):
+        for ex in range(len(self.iterVal)):
             # todo minx_idx คือ ตัวแปรที่หาจำนวนที่น้อยที่สุด โดย เริ่มต้นที่ตำแหน่งตัวเอง
             min_idx = ex
             # todo ex+1 คือการเริ่มต้นที่ตำแหน่งถัดไปของ ex
-            for lt in range(ex+1, len(self.get_array)):
+            for lt in range(ex+1, len(self.iterVal)):
                # todo เปรียบระหว่าง ตัวที่ min idx มากกกว่า ตัวที่ lt หรือไม่
-                if self.get_array[min_idx] > self.get_array[lt]:
+                if self.iterVal[min_idx] > self.iterVal[lt]:
                     min_idx = lt  # todo ให้ min_idx มีค่าเท่ากับ lt เผื่อใช้หาตัวแหน่งที่น้อยกว่าในรอบถัดไป
             # todo สลับตำแหน่ง ตัวที่ ex(ตัวที่ใช้เปรียบเทียบ) และ ตัวที่ min_idx(ค่าน้อยที่สุด)
-            self.get_array[ex], self.get_array[min_idx] = self.get_array[min_idx], self.get_array[ex]
+            maxVal = self.iterVal[ex]
+            minVal = self.iterVal[min_idx]
+            self.iterVal[ex], self.iterVal[min_idx] = minVal,maxVal
 
     #! Insert Method
     def insert_ele(self, idx: int, ele):
         # todo imp_var เก็บสมาชิค array ตั้งแต่ idx ถึง ตัวสุดท้าย
-        imp_var = self.get_array[idx:]
-        # todo ให้ self.get_array เก็บสมาชิกตั้งแต่ 0 ถึง idx-1 แทน
-        self.get_array = self.get_array[:idx]
-        # todo เพิ่ม ele เข้าต่อท้ายใน self.get_array
-        self.get_array.append(ele)
-        # todo รวม self.get_array กับ imp_var เข้าด้วยกัน
-        self.get_array = self.get_array+imp_var
+        imp_var = self.iterVal[idx:]
+        # todo ให้ self.iterVal เก็บสมาชิกตั้งแต่ 0 ถึง idx-1 แทน
+        self.iterVal = self.iterVal[:idx]
+        # todo เพิ่ม ele เข้าต่อท้ายใน self.iterVal
+        self.iterVal.append(ele)
+        # todo รวม self.iterVal กับ imp_var เข้าด้วยกัน
+        self.iterVal = self.iterVal+imp_var
 
     #! Delete Method
     def delete_ele(self, ele):
-        self.get_array = list(filter(lambda s: s != ele, self.get_array))
+        # self.iterVal = list(filter(lambda s: s != ele, self.iterVal))
+        self.iterVal = [i for i in self.iterVal if i != ele]
 
     #! Search Method
     def search_ele(self, ele, start=0):
-        return self.get_array.index(ele, start, len(self.get_array))
+        return self.iterVal.index(ele, start, len(self.iterVal))
 
     #! Min element
     @property
     def min_ele(self):
         self.bubble_sort
-        return self.get_array[0]
+        return self.iterVal[0]
 
     #! Max element
     @property
     def max_ele(self):
         self.bubble_sort
-        return self.get_array[len(self.get_array)-1]
+        return self.iterVal[len(self.iterVal)-1]
 
 
 if __name__ == '__main__':
     with open('data.txt', 'r') as f:
-        read_num = f.read().split(' ')[:12]
+        read_num = f.read().split(' ')[0:12]
         num_lst = list(map(lambda s: int(s), read_num))
-        print(f'Unort : {num_lst}')
         arr_ins = Array(num_lst)
-        arr_ins.insertion_sort 
-        print(arr_ins.search_ele(66))
-        print('Sorted :', arr_ins.get_array)
+        print(arr_ins.iterVal)
+        arr_ins.BubbleSort
+        # print(arr_ins.search_ele(66))
+        print(arr_ins.iterVal)
--- testmathplot.py
+++ testmathplot.py
@@ -1,8 +1,28 @@
-def insert(idx:int,ele,lst:list):
-    imp_var = lst[idx:]
-    lst = lst[:idx]
-    lst.append(ele)
-    lst = lst+imp_var
-    return lst
-p = []
-print(insert(0,2,p))
+# def insert(idx:int,ele,lst:list):
+#     imp_var = lst[idx:]
+#     lst = lst[:idx]
+#     lst.append(ele)
+#     lst = lst+imp_var
+#     return lst
+# p = []
+# print(insert(0,2,p))
+import matplotlib.pyplot as plt
+import numpy as np
+  
+# x axis values
+x = np.array([0,1,2,3,4,5,6,7,8,9,10])
+# corresponding y axis values
+y = np.array([0,1.006,2.012,3.018,4.024,5.030,6.036,7.042,8.048,9.054,10.60])
+  
+# plotting the points 
+plt.plot(x, y,'o:r')
+plt.title('R = 1kΩ')
+# naming the x axis
+plt.xlabel('V(v)')
+# naming the y axis
+plt.ylabel('I(A)')
+  
+# giving a title to my graph
+  
+# function to show the plot
+plt.show()

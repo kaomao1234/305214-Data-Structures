a022bc42597639fc3bd7c0644d2b0ab9c4d91362
kaomao1234 <boripats63@nu.ac.th>
2021-07-26 23:11:56 +0700

    add all

--- lab-recusionV2.py
+++ lab-recusionV2.py
@@ -1,4 +1,5 @@
 def factorial(num: int):
+    print('this recursion no.{}'.format(num))
     if num == 1:
         return 1
     return num * factorial(num-1)
@@ -13,7 +14,7 @@ def powFunc(num: int, powVar: int):
 def fibonance(num: int, result=[], start=0):
     if len(result) < 2:
         result.append(start)
-        return fibonance(num, result, start+1)
+        fibonance(num, result, start+1)
     if len(result) == num:
         return result
     result.append(result[start-2]+result[start-1])
@@ -25,9 +26,11 @@ def sumArray(array: list, idx=0):
         return array[idx]
     return array[idx] + sumArray(array, idx+1)
 
-if __name__ == '__main__':
-    print(factorial(10))
-    print(powFunc(4,3))
-    print(fibonance(10))
-    print(sumArray([4,5,3,7,8,9]))
 
+if __name__ == '__main__':
+    # print(factorial(10))
+    # print(powFunc(4,3))
+    # print(fibonance(10))
+    # print(sumArray([4, 5, 3, 7, 8, 9]))
+    print(3) == 3
+    print(3)

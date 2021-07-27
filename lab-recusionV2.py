622af1d1ead1368db4002ef05b187e5c586e2ffd
kaomao1234 <boripats63@nu.ac.th>
2021-07-27 16:40:14 +0700

    add all

--- lab-recusionV2.py
+++ lab-recusionV2.py
@@ -11,25 +11,31 @@ def powFunc(num: int, powVar: int):
     return num*pow(num, powVar-1)
 
 
-def fibonance(num: int, result=[], start=0):
-    if len(result) < 2:
-        result.append(start)
-        fibonance(num, result, start+1)
+def fib(num: int, result=[], start=0):
+    print(f'number of element {len(result)} : ', result)
+    if num < 0:
+        return []
     elif len(result) == num:
         return result
-    print(result)
-    result.append(result[start-2]+result[start-1])
-    return fibonance(num, result)
+    elif len(result) > 2:
+        result.append(result[len(result)-2]+result[len(result)-1])
+    else:
+        result.append(start)
+    return fib(num, result, start+1)
 
 
 def sumArray(array: list, idx=0):
     if idx == len(array)-1:
         return array[idx]
-    return array[idx] + sumArray(array, idx+1)
+    elif len(array) >= 2:
+        return array[idx] + sumArray(array, idx+1)
+    else:
+        array = array[0] if 0 < len(array) < 2 else 0
+        return array
 
 
 if __name__ == '__main__':
     # print(factorial(10))
     # print(powFunc(4,3))
-    print(fibonance(10))
+    print('ผลรวม', sumArray(fib(2)))
     # print(sumArray([4, 5, 3, 7, 8, 9]))

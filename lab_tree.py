69fbd1d45ac228e8ed2898d3b99d92bf8220daf5
kaomao1234 <boripats63@nu.ac.th>
2021-08-01 18:35:29 +0700

    Create lab_tree.py

---
+++ lab_tree.py
@@ -0,0 +1,14 @@
+import time
+class Node:
+    def __init__(self,array):
+        self.array = array
+    def filess(self,node):
+        temp = list(filter(lambda s : s<node,self.array))
+        return temp
+    def filthan(self,node):
+        temp = list(filter(lambda s : s>node,self.array))
+        return temp
+
+if __name__ == '__main__':
+    obj = Node([4,6,3,1,5,8,9]) 
+    c(obj.array[0])

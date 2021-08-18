0c01eeb0e08ba06ac7fc170d6adb316d7049f535
kaomao1234 <boripats63@nu.ac.th>
2021-08-18 17:48:53 +0700

    add all

---
+++ lab_doubleLinkList.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoubleLL:
+    def __init__(self,head):
+        self.head = head
+        
+    def add(self):
+        pass
+    def __add(self):
+        pass
+Object  = DoubleLL(Node('A'))
--- lab_linklist.py
+++ lab_linklist.py
@@ -10,28 +10,38 @@ class SLList:
         self.first = None
 
     def add(self, data):
-        self.addNode(data, self.first)
+        self.__addNode__(data, self.first)
 
     def search(self, ele):
-        return self.searchNode(ele, self.first)
+        return self.__searchNode__(ele, self.first)
 
     def show(self):
-        self.showNode(self.first)
+        self.__showNode__(self.first)
 
     def len(self):
-        return self.lenNode(self.first)
+        return self.__lenNode__(self.first)
 
     def delete(self, key):
-        self.first = self.delNode(key, self.first)
+        self.first = self.__delNode__(key, self.first)
+
+    def delV2(self, slist, key):
+        if slist.data == key:
+            print(slist.data == key)
+            slist = None
+        elif slist.next.data == key:
+            ptr = slist.next.next
+            slist.next = ptr
+        else:
+            self.delV2(slist.next, key)
 
     def push(self, new_data):
         if type(Node()) == new_data:
             new_data.next = self.first
-            self.head = new_data
+            self.first = new_data
         else:
             new_data = Node(new_data)
             new_data.next = self.first
-            self.head = new_data
+            self.first = new_data
 
     def insert(self, idx, data):
         # todo ตัวแปรที่เก็บตั้งแต่ Node แรก ถึง Node idx
@@ -43,7 +53,7 @@ class SLList:
             st_toidx = st_toidx.next  # todo ตัวแปรที่เก็บตั้งแต่ Node แรก ถึง Node idx
             new_node.next = prev_node  # todo  นำ new_node ไปต่อกับ Node ที่เหลือ
             # todo นำ new_node มาต่อกับ st_toidx
-            self.addNode(new_node, st_toidx)
+            self.__addNode__(new_node, st_toidx)
             self.first = st_toidx
 
         elif idx == 0:  # todo ถ้า idx เท่ากับ 0
@@ -51,23 +61,23 @@ class SLList:
             self.first = new_node
 
     # todo สร้าง fuction delNode กำหนดและ new_node เป็น node ใหม่
-    def delNode(self, key, slist: Node, new_node=Node()):
+    def __delNode__(self, key, slist: Node, new_node=Node()):
         if slist == None:  # todo ถ้าslist == None
             new_node = new_node.next
             return new_node
         elif slist.data != key:  # todo ถ้า varที่ต้องการลบ ไม่เท่ากับ data
             # todo เพิ่มข้อมูลเข้าไปใน new_node
             self.addNode(slist.data, new_node)
-            return self.delNode(key, slist.next, new_node)
+            return self.__delNode__(key, slist.next, new_node)
         else:
-            return self.delNode(key, slist.next, new_node)
-    
+            return self.__delNode__(key, slist.next, new_node)
+
     # todo function ที่เก็บNode ตั้งแต่ Node แรก ถึง Node ที่ idx
     def headtoIdx(self, idx, head: Node, newNode: Node):
         if idx == 0:
             return newNode
         else:
-            self.addNode(head.data, newNode)
+            self.__addNode__(head.data, newNode)
             return self.headtoIdx(idx-1, head.next, newNode)
 
     # todo function ที่เก็บตั้งแต่ Nodeที่ idx ถึง Node สุดท้าย
@@ -77,33 +87,33 @@ class SLList:
         else:
             return self.idxtoLast(idx-1, head.next)
 
-    def lenNode(self, slist: Node, var=0):
+    def __lenNode__(self, slist: Node, var=0):
         if slist == None:
             return var
         else:
-            return self.lenNode(slist.next, var+1)
+            return self.__lenNode__(slist.next, var+1)
 
-    def showNode(self, slist: Node):
+    def __showNode__(self, slist: Node):
         if slist == None:
             return
         print(slist.data, ' --> ', end='')
-        self.showNode(slist.next)
+        self.__showNode__(slist.next)
 
-    def searchNode(self, data, slist: Node, start=0):
+    def __searchNode__(self, data, slist: Node, start=0):
         if slist == None:
             return False
         elif slist.data == data:
             return start
-        return self.searchNode(data, slist.next, start+1)
+        return self.__searchNode__(data, slist.next, start+1)
 
-    def addNode(self, data, slist: Node):
+    def __addNode__(self, data, slist: Node):
         if slist.next == None:
             if type(data) == type(Node()):
                 slist.next = data
             else:
                 slist.next = Node(data)
         else:
-            self.addNode(data, slist.next)
+            self.__addNode__(data, slist.next)
 
 
 if __name__ == '__main__':
@@ -112,5 +122,6 @@ if __name__ == '__main__':
     SinglyObj.add('C')
     SinglyObj.add('M')
     SinglyObj.add('P')
+    SinglyObj.delV2(SinglyObj.first, 'A')
     # SinglyObj.insert(0, 'X')
     SinglyObj.show()

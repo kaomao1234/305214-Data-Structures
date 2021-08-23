48b68760984d174709b3c9e85a5f3e9b39174a65
kaomao1234 <boripats63@nu.ac.th>
2021-08-23 15:15:09 +0700

    Update JumpSearch.py

--- JumpSearch.py
+++ JumpSearch.py
@@ -1,28 +1,30 @@
-# Python3 code to implement Jump Search
-from math import *
+arr = [0, 1, 1, 2, 3, 5, 8, 13, 21,
+       34, 55, 89, 144, 233, 377, 610]
+x = 610
 
 
-def jumpSearch(arr, data):
-    start = 0
-    end = len(arr)
-    jump_size = round(sqrt(end))
-    for i in range(start, end):
-        print(i)
-        jump = i+jump_size-1
-        prev_ar = jump-1
-        next_ar = jump + 1
-        if next_ar >= end:
-            raise IndexError('list index out of range')
-        elif arr[i] == data and i == 0:
-            return i
-        elif arr[prev_ar] == data:
-            return prev_ar
-        elif arr[jump] == data:
-            return jump
-        elif arr[next_ar] == data:
-            return next_ar
+def jumpSearch(arr, key):
+    size = len(arr)  # todo วัดความยาวของ array
+    jump_size = round(size**1/2)  # todo หาขั้นในแต่ละครั้ง
+    last_step = 0  # todo เก็บค่าของ jump_size ในกรณีที่ arr[jumpsize] > key
+    for i in range(0, size):
+        step = i*(jump_size-1)  # todo คำนวณค่า step ในแต่ละ รอบ
+        if step >= size:
+            return -1
+        # todo ถ้า step เท่ากับ key ส่งค่ากลับเป็น step(ตำแหน่งในarr)
+        elif arr[step] == key:
+            return step
+        # todo เพิ่ม stepไปอีก step แล้วถ้า arr[step] > key กำหนดตัวแปร last_step เก็บค่า step
+        elif arr[step+(jump_size-1)] > key:
+            last_step = step
+            break
+    for j in range(last_step, size):
+        # todo ทำการเทียบ arr[j] กับ key โดยเริ่มหาจากตำแหน่งที่ last_step ถึงตำแหน่งสุดท้าย
+        if arr[j] == key:
+            return j  # todo ส่งค่ากลับเป็น j(ตำแหน่งในarr)
 
 
-# Driver code to test function
-arr = [3, 2, 4, 9, 1, 5, 2, 6]
-print(jumpSearch(arr, 5))
+# print(jumpSearch(arr, 620))
+while score_in > 50:
+    score_in = int(input())
+print('end')
